79780cc1b7a5e7ded5f67809c1b3e93b092f136b
imran-kkz <imranrali1@gmail.com>
2020-07-22 23:20:07 -0400

    init mvn archetype

---
+++ learning-maven/src/main/java/imrankkz/App.java
@@ -0,0 +1,13 @@
+package imrankkz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
